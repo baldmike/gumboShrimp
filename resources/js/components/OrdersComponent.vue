e76350e49a465140b1b05d15bfcfb1d0591a1a6d
Bald Mike <baldmike312@gmail.com>
2023-05-22 21:07:27 -0500

    updates shop component

--- resources/js/components/OrdersComponent.vue
+++ resources/js/components/OrdersComponent.vue
@@ -1,63 +1,27 @@
 <template>
     <b-container fluid class="container-box">
-        <b-row id="card-body">
-            <b-col lg="3" md="4" sm="6" v-for="order in orders" :key="order.id">
-                <b-card class="fadeIn">          
-                    <b-row class="text-center">
-                        <b-col>
-                            <b-card-text style="font-size: .7em"> Order Id: {{ order.id }} </b-card-text>
-                        </b-col>
-                    </b-row>
-                    <b-row class="text-center">
-                        <b-col>
-                            <b-card-text style="font-size: .7em">Name: {{ order.name }} </b-card-text>
-                        </b-col>
-                    </b-row>
-                    <b-row class="text-center">
-                        <b-col>
-                            <b-card-text style="font-size: .7em">Email: {{ order.email }} </b-card-text>
-                        </b-col>
-                    </b-row>
-                    <b-row class="text-center">
-                        <b-col>
-                            <b-card-text style="font-size: .7em"> Address: {{ order.address }} </b-card-text>
-                        </b-col>
-                    </b-row>
-                    <b-row class="text-center">
-                        <b-col>
-                            <b-card-text style="font-size: .7em"> City: {{ order.city }} </b-card-text>
-                        </b-col>
-                    </b-row>
-                    <b-row class="text-center">
-                        <b-col>
-                            <b-card-text style="font-size: .7em"> State: {{ order.state }} </b-card-text>
-                        </b-col>
-                    </b-row>
-                    <b-row class="text-center">
-                        <b-col>
-                            <b-card-text style="font-size: .7em">Zip: {{ order.zip }} </b-card-text>
-                        </b-col>
-                    </b-row>
-                    <b-row v-for='(item, id) in order.order_item' :key="id" class="text-center">
-                        <b-col>
-                            {{ item["quantity"] }} -- {{ item["item_name"] }}
-                        </b-col>
-                    </b-row>
-                    <b-row class="text-center bottom">
-                        <b-col>
-                            <b-card-text>amount: {{ order.amount }} </b-card-text>
-                        </b-col>
-                    </b-row>
-                    <b-row class="text-center bottom">
-                        <b-col>
-                            <b-button @click.prevent="fulfillOrder(order.id)">Fulfill</b-button>
-                        </b-col>
-                    </b-row>
-                </b-card>
-            </b-col>
-        </b-row>
+      <b-row id="card-body">
+        <b-col lg="3" md="4" sm="6" v-for="order in orders" :key="order.id">
+          <b-card class="fadeIn" style="padding: 1.5rem;">
+            <b-card-text class="order-id">Order Id: {{ order.id }}</b-card-text>
+            <b-card-text class="order-info">Name: {{ order.name }}</b-card-text>
+            <b-card-text class="order-info">Email: {{ order.email }}</b-card-text>
+            <b-card-text class="order-info">Address: {{ order.address }}</b-card-text>
+            <b-card-text class="order-info">City: {{ order.city }}</b-card-text>
+            <b-card-text class="order-info">State: {{ order.state }}</b-card-text>
+            <b-card-text class="order-info">Zip: {{ order.zip }}</b-card-text>
+            <b-row v-for="(item, id) in order.order_item" :key="id" class="text-center">
+              <b-col>
+                {{ item["quantity"] }} -- {{ item["item_name"] }}
+              </b-col>
+            </b-row>
+            <b-card-text class="order-amount">Amount: {{ order.amount }}</b-card-text>
+            <b-button class="fulfill-button" @click.prevent="fulfillOrder(order.id)">Fulfill</b-button>
+          </b-card>
+        </b-col>
+      </b-row>
     </b-container>
-</template>
+  </template>
 
 <script>
     import { mapGetters, mapActions } from "vuex";
@@ -109,3 +73,44 @@
         }
     }
 </script>
+
+<style scoped>
+.container-box {
+  background-color: white;
+  margin-top: 2em;
+  padding-bottom: 2em;
+}
+
+.b-card {
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  transition: 0.3s;
+  text-align: center;
+}
+
+.b-card:hover {
+  box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
+}
+
+.order-id {
+  font-size: 1em;
+  font-weight: bold;
+  margin-bottom: 0.5rem;
+}
+
+.order-info {
+  font-size: 0.9em;
+  margin-bottom: 0.3rem;
+}
+
+.order-amount {
+  font-weight: bold;
+  margin-top: 0.5rem;
+  margin-bottom: 0.5rem;
+}
+
+.fulfill-button {
+  background-color: #3498db;
+  color: white;
+  margin-top: 0.5rem;
+}
+</style>
--- resources/js/components/ShopComponent.vue
+++ resources/js/components/ShopComponent.vue
@@ -1,20 +1,20 @@
 <template>
-    <b-container fluid class="container-box">
-      <b-row>
-        <b-col offset="4" lg="4" md="6" sm="8">
-          <b-input-group class="mt-3">
-            <template v-slot:append>
-              <b-button class="btn">
-                <li class="fa fa-search"></li>
-              </b-button>
-            </template>
-            <b-form-input v-model="search" placeholder="Search for your item..."></b-form-input>
-          </b-input-group>
-        </b-col>
-      </b-row>
-      <br />
-  
-      <b-row id="card-body">
+  <b-container fluid class="container-box">
+    <b-row>
+      <b-col offset="4" lg="4" md="6" sm="8">
+        <b-input-group class="mt-3">
+          <template v-slot:append>
+            <b-button class="btn">
+              <li class="fa fa-search"></li>
+            </b-button>
+          </template>
+          <b-form-input v-model="search" placeholder="Search for your item..."></b-form-input>
+        </b-input-group>
+      </b-col>
+    </b-row>
+    <br />
+
+    <b-row id="card-body">
       <b-col lg="3" md="4" sm="6" v-for="item in filteredItems" :key="item.id">
         <b-card class="fadeIn" v-if="item.image" :img-src="item.image" img-alt="Item image" img-height="300" img-width="300" :title="item.item_name">
           <b-row class="text-center">
@@ -57,277 +57,277 @@
         </b-card>
       </b-col>
     </b-row>
-  
-      <!-- View update Item Modal -->
-      <b-modal id="editModal" hide-footer no-close-on-backdrop>
-        <form>
-          <div class="form-group">
-            <label for="image" class="col-sm-2 control-label">Image</label>
-            <b-form-file
-              id="image"
-              accept="image/*"
-              v-model="updateItem.itemImage"
-              placeholder="Choose an image..."
-              @change="onImageChange"
-            ></b-form-file>
-          </div>
-  
-          <b-col cols="6" style="margin-top: 1rem;">
-            <img v-if="url" :src="url" width="420" alt="Uploaded image">
-          </b-col>
-  
-          <div class="form-group">
-            <label for="name">Item</label>
-            <input v-model="updateItem.itemName" type="text" class="form-control" id="name" />
-          </div>
-          <div class="form-group">
-            <label for="price">Price</label>
-            <input v-model="updateItem.itemPrice" type="text" class="form-control" id="price" />
-          </div>
-  
-          <div class="form-group">
-            <label for="description">Item Description</label>
-            <input v-model="updateItem.itemDescription" type="text" class="form-control" id="description" />
-          </div>
-          <div class="form-group">
-            <label for="number_available">Number Available</label>
-            <input v-model="updateItem.numberAvailable" type="number" class="form-control" id="numberAvailable" />
-          </div>
-          <div class="form-group" v-if="updateItem.size">
-            <label for="size">Size</label>
-            <input v-model="updateItem.itemSize" type="text" class="form-control" id="size" />
-          </div>
-          <b-button class="mt-3" block @click.prevent="editItem">Edit Item</b-button>
-        </form>
-      </b-modal>
-    </b-container>
-  </template>
-  
-  <script>
-  import { mapGetters, mapActions } from "vuex";
-  
-  export default {
-    name: "shop",
-    data() {
-      return {
-        search: "",
-        items: [],
-        updateItem: {
-          itemName: "",
-          itemDescription: "",
-          itemImage: null,
-          itemPrice: "",
-          numberAvailable: "",
-          itemSize: "",
-          sent: false,
-        },
-        url: null,
-      };
-    },
-  
-    methods: {
-      loadItems() {
-        axios
-          .get("/api/items")
-          .then((response) => {
-            this.items = response.data.data;
-            console.log(this.items);
-            console.log("Items have successfully loaded");
-          })
-          .catch((error) => {
-            console.log("ERROR ON LOAD ITEMS. Error-->");
-            console.log(error);
-          });
+
+    <!-- View update Item Modal -->
+    <b-modal id="editModal" hide-footer no-close-on-backdrop>
+      <form>
+        <div class="form-group">
+          <label for="image" class="col-sm-2 control-label">Image</label>
+          <b-form-file
+            id="image"
+            accept="image/*"
+            v-model="updateItem.itemImage"
+            placeholder="Choose an image..."
+            @change="onImageChange"
+          ></b-form-file>
+        </div>
+
+        <b-col cols="6" style="margin-top: 5rem;">
+          <img v-if="url" :src="url" width="420" alt="Uploaded image">
+        </b-col>
+
+        <div class="form-group">
+          <label for="name">Item</label>
+          <input v-model="updateItem.itemName" type="text" class="form-control" id="name" />
+        </div>
+        <div class="form-group">
+          <label for="price">Price</label>
+          <input v-model="updateItem.itemPrice" type="text" class="form-control" id="price" />
+        </div>
+
+        <div class="form-group">
+          <label for="description">Item Description</label>
+          <input v-model="updateItem.itemDescription" type="text" class="form-control" id="description" />
+        </div>
+        <div class="form-group">
+          <label for="number_available">Number Available</label>
+          <input v-model="updateItem.numberAvailable" type="number" class="form-control" id="numberAvailable" />
+        </div>
+        <div class="form-group" v-if="updateItem.size">
+          <label for="size">Size</label>
+          <input v-model="updateItem.itemSize" type="text" class="form-control" id="size" />
+        </div>
+        <b-button class="mt-3" block @click.prevent="editItem">Edit Item</b-button>
+      </form>
+    </b-modal>
+  </b-container>
+</template>
+
+<script>
+import { mapGetters, mapActions } from "vuex";
+
+export default {
+  name: "shop",
+  data() {
+    return {
+      search: "",
+      items: [],
+      updateItem: {
+        itemName: "",
+        itemDescription: "",
+        itemImage: null,
+        itemPrice: "",
+        numberAvailable: "",
+        itemSize: "",
+        sent: false,
       },
-      showUpdateItemModal(item) {
-        this.url = item.itemImage;
-        this.updateItem.itemImage = item.itemImage;
-        this.updateItem.itemName = item.item_name;
-        this.updateItem.itemPrice = item.price;
-        this.updateItem.numberAvailable = item.number_available;
-        this.updateItem.itemDescription = item.description;
-        this.updateItem.id = item.id;
-  
-        this.$bvModal.show("editModal");
-      },
-      editItem() {
-        console.log(this.updateItem.id);
-  
-        let formData = new FormData();
-  
-        Object.keys(this.updateItem).forEach((key) => {
-          formData.append(key, this.updateItem[key]);
+      url: null,
+    };
+  },
+
+  methods: {
+    loadItems() {
+      axios
+        .get("/api/items")
+        .then((response) => {
+          this.items = response.data.data;
+          console.log(this.items);
+          console.log("Items have successfully loaded");
+        })
+        .catch((error) => {
+          console.log("ERROR ON LOAD ITEMS. Error-->");
+          console.log(error);
         });
-  
-        formData.append("_method", "PUT");
-  
-        let self = this;
+    },
+    showUpdateItemModal(item) {
+      this.url = item.itemImage;
+      this.updateItem.itemImage = item.itemImage;
+      this.updateItem.itemName = item.item_name;
+      this.updateItem.itemPrice = item.price;
+      this.updateItem.numberAvailable = item.number_available;
+      this.updateItem.itemDescription = item.description;
+      this.updateItem.id = item.id;
+
+      this.$bvModal.show("editModal");
+    },
+    editItem() {
+      console.log(this.updateItem.id);
+
+      let formData = new FormData();
+
+      Object.keys(this.updateItem).forEach((key) => {
+        formData.append(key, this.updateItem[key]);
+      });
+
+      formData.append("_method", "PUT");
+
+      let self = this;
+      axios
+        .post("/api/items/" + this.updateItem.id, formData)
+        .then((response) => {
+          this.$bvModal.hide("editModal");
+
+          setTimeout(
+            this.$notify({
+              group: "notifications",
+              type: "success",
+              title: "Success!",
+              text: "Item successfully updated.",
+              duration: 15000,
+              width: "100%",
+            }),
+            9000
+          );
+          this.loadItems();
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+    deleteItem(id, name) {
+      if (confirm("Are you sure you want to delete " + name + "?")) {
         axios
-          .post("/api/items/" + this.updateItem.id, formData)
-          .then((response) => {
-            this.$bvModal.hide("editModal");
-  
+          .delete("/api/items/" + id)
+          .then(({ data }) => {
             setTimeout(
               this.$notify({
                 group: "notifications",
                 type: "success",
                 title: "Success!",
-                text: "Item successfully updated.",
+                text: name + " successfully deleted.",
                 duration: 15000,
                 width: "100%",
               }),
-              9000
+              15000
             );
+
             this.loadItems();
           })
           .catch((error) => {
             console.log(error);
           });
-      },
-      deleteItem(id, name) {
-        if (confirm("Are you sure you want to delete " + name + "?")) {
-          axios
-            .delete("/api/items/" + id)
-            .then(({ data }) => {
-              setTimeout(
-                this.$notify({
-                  group: "notifications",
-                  type: "success",
-                  title: "Success!",
-                  text: name + " successfully deleted.",
-                  duration: 15000,
-                  width: "100%",
-                }),
-                15000
-              );
-  
-              this.loadItems();
-            })
-            .catch((error) => {
-              console.log(error);
-            });
-        }
-      },
-      addToCart(item) {
+      }
+    },
+    addToCart(item) {
+      this.$notify({
+        group: "notifications",
+        type: "success",
+        title: "Item Added",
+        text: "Item added to cart.",
+        duration: 8000,
+        width: "100%",
+      });
+      this.$store.dispatch("addToCart", item);
+    },
+    updateProfile(e) {
+      let file = e.target.files[0];
+      let reader = new FileReader();
+      if (file["size"] < 2111775) {
+        reader.onloadend = (file) => {
+          this.updateItem.image = reader.result;
+          console.log(this.updateItem.image);
+          console.log("IMAGE;");
+        };
+        reader.readAsDataURL(file);
+      } else {
         this.$notify({
           group: "notifications",
-          type: "success",
-          title: "Item Added",
-          text: "Item added to cart.",
-          duration: 8000,
+          type: "warning",
+          title: "Failure",
+          text: "File too large",
+          duration: 15000,
           width: "100%",
         });
-        this.$store.dispatch("addToCart", item);
-      },
-      updateProfile(e) {
-        let file = e.target.files[0];
-        let reader = new FileReader();
-        if (file["size"] < 2111775) {
-          reader.onloadend = (file) => {
-            this.updateItem.image = reader.result;
-            console.log(this.updateItem.image);
-            console.log("IMAGE;");
-          };
-          reader.readAsDataURL(file);
-        } else {
-          this.$notify({
-            group: "notifications",
-            type: "warning",
-            title: "Failure",
-            text: "File too large",
-            duration: 15000,
-            width: "100%",
-          });
-        }
-      },
-      onImageChange(e) {
-        const file = e.target.files[0];
-        this.url = URL.createObjectURL(file);
-        this.updateItem.image = file;
-      },
+      }
     },
-    computed: {
-      totalPrice() {
-        let total = 0;
-        for (let item of this.$store.state.cart) {
-          total += item.totalPrice;
-          console.log(total);
-        }
-        return total.toFixed(2);
-      },
-      filteredItems() {
-        let self = this;
-  
-        if (this.search) {
-          return this.items.filter(
-            (item) =>
-              item.item_name.toLowerCase().indexOf(self.search.toLowerCase()) >=
-              0
-          );
-        }
-  
-        return this.items;
-      },
-      ...mapGetters([
-        "isAuthenticated",
-        "currentUser",
-        "cart",
-        "cartCount",
-      ]),
+    onImageChange(e) {
+      const file = e.target.files[0];
+      this.url = URL.createObjectURL(file);
+      this.updateItem.image = file;
     },
-    mounted() {
-      this.loadItems();
-      console.log("Shop Component mounted.");
+  },
+  computed: {
+    totalPrice() {
+      let total = 0;
+      for (let item of this.$store.state.cart) {
+        total += item.totalPrice;
+        console.log(total);
+      }
+      return total.toFixed(2);
     },
-  };
-  </script>
-  
-  <style scoped>
-  .container-box {
-    background-color: white;
-    margin-top: 2em;
-    padding-bottom: 2em;
-  }
-  
-  .b-bottom {
-    position: relative;
-    top: 10;
-    left: 0;
-    right: 0;
-    text-align: center;
-  }
-  
-  .brandName {
-    color: white;
-  }
-  
-  .card-footer {
-    position: absolute;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    margin-left: auto;
-    margin-right: auto;
-    margin-bottom: auto;
-    text-align: center;
-  }
-  
-  .modal-backdrop {
-    opacity: 0;
-    transition: opacity 0.8s;
-  }
-  
-  .modal-backdrop.in {
-    opacity: 0.7;
-  }
-  
-  .card-body {
-    flex: 1 1 auto;
-    height: 300px !important;
-    padding: 1.25rem;
-  }
+    filteredItems() {
+      let self = this;
 
-  .item-description {
+      if (this.search) {
+        return this.items.filter(
+          (item) =>
+            item.item_name.toLowerCase().indexOf(self.search.toLowerCase()) >=
+            0
+        );
+      }
+
+      return this.items;
+    },
+    ...mapGetters([
+      "isAuthenticated",
+      "currentUser",
+      "cart",
+      "cartCount",
+    ]),
+  },
+  mounted() {
+    this.loadItems();
+    console.log("Shop Component mounted.");
+  },
+};
+</script>
+
+<style scoped>
+.container-box {
+  background-color: white;
+  margin-top: 2em;
+  padding-bottom: 2em;
+}
+
+.b-bottom {
+  position: relative;
+  top: 10;
+  left: 0;
+  right: 0;
+  text-align: center;
+}
+
+.brandName {
+  color: white;
+}
+
+.card-footer {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  margin-left: auto;
+  margin-right: auto;
+  margin-bottom: auto;
+  text-align: center;
+}
+
+.modal-backdrop {
+  opacity: 0;
+  transition: opacity 0.8s;
+}
+
+.modal-backdrop.in {
+  opacity: 0.7;
+}
+
+.card-body {
+  flex: 1 1 auto;
+  height: 300px !important;
+  padding: 1.25rem;
+}
+
+.item-description {
   font-size: 0.7em;
 }
 
@@ -356,5 +356,8 @@
 .btn-add-to-cart {
   background-color: #27ae60;
 }
-  </style>
-  
+
+.col-sm-6 {
+  margin-bottom: 40px;
+}
+</style>
